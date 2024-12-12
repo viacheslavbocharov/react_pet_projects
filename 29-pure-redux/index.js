f5897794d8b8b99fe7215cd76e3422cd84221fc5
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-12-12 15:58:57 +0000

    add 29-pure-reducer proj

---
+++ 29-pure-redux/index.js
@@ -0,0 +1,35 @@
+import store from './redux/store.js'
+import { addCurrentTime } from './redux/actionCreators.js'
+import { clearTimes } from './redux/actionCreators.js'
+
+const addTimeBtn = document.getElementById('addTime')
+addTimeBtn.addEventListener('click', () => {
+    store.dispatch(addCurrentTime())
+})
+
+const clearTimesBtn = document.getElementById('clearTimes')
+clearTimesBtn.addEventListener('click', () => {
+    store.dispatch(clearTimes())
+})
+
+const timesList = document.getElementById('timesList')
+
+store.subscribe(() => {
+    timesList.innerHTML = ''
+    const times = store.getState()
+    times.forEach((time) => {
+        let li = document.createElement('li')
+        li.innerText = time
+        timesList.appendChild(li)
+    })
+})
+
+// const unsubscribe = store.subscribe(() => console.log(`Redux store jusy changed! ${store.getState()}`))
+
+// store.dispatch({ type: 'ADD_CURRENT_TIME', payload: '11:30:00' })
+
+// unsubscribe()
+
+// store.dispatch({ type: 'ADD_CURRENT_TIME', payload: '11:32:00' })
+
+// store.dispatch({ type: 'CLEAR_ALL_TIMES' })
---
+++ 29-pure-redux/redux/reducer.js
@@ -0,0 +1,15 @@
+import * as actionTypes from './actionTypes.js'
+const initialState = []
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case actionTypes.ADD_CURRENT_TIME:
+            return [...state, action.payload]
+        case actionTypes.CLEAR_ALL_TIMES:
+            return []
+        default:
+            return state;
+    }
+}
+
+export default reducer
